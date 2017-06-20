58d63e3fc2dad9108285604d5598a21ff6beb3b7
juanraz <locomaestro@gmail.com>
2017-06-20 00:24:47 -0400

    first commit, container, login module, basic navigation is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,38 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { MessageComponent } from './message/message.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import { MessageItemComponent } from './message-item/message-item.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'chat',      component: ChatComponent },
+  {path:'login',      component: LoginComponent },
+  { path: '',         redirectTo:'/login', pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    LoginComponent,
+    HeaderComponent,
+    MessageComponent,
+    MessageListComponent,
+    MessageItemComponent,
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    Angular2FontawesomeModule,
     BrowserModule,
     FormsModule,
     HttpModule
